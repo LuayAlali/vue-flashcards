7169611bf3f6f9da50e3dbe8b6d287657ba2a04f
Luay Alali <luayalali@gmail.com>
2020-10-19 17:04:26 -0700

    looping though card in cards.

--- s4v1/app.js
+++ s4v1/app.js
@@ -20,8 +20,16 @@ const cards = [
       back: 'Dr. Evelyn Boyd Granville',
       flipped: false,
     },
-  ]; 
+  ];
 
+//Display our data
+//  on Click: flip cards back and forth
+// get info for new cards from te user
+// Add new card when user hits enter or clicks button
+//  Delete cards
+// Animate card flip
+// Display an error message if form fields are blank
+ 
   new Vue({
     el: '#flashcard-app',
     data: {
